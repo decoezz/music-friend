5f5660e8a038c1e3d7d6119b2b9ffb7a1c58fa35
decoezz <45102309+decoezz@users.noreply.github.com>
2019-01-04 18:17:29 +0200

    Delete config1.js

--- config1.js
+++
@@ -1,5 +0,0 @@
-exports.TOKEN = 'NTMwNzcxNTY1MTIyNDIwNzQ2.DxERqg.zdPE-bbIFNfWxyboyVB-7C6MXiA'; // حط توكن البوت هنا
-
-exports.PREFIX = '2';//برفكس البوت هو يلي بيشغل لاغاني
-
-exports.GOOGLE_API_KEY = 'AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8';
